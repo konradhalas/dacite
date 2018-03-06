0afae0efc6a9d202df33e2319a2b53b0853eb598
konradhalas <halas.konrad@gmail.com>
2018-03-06 20:03:25 +0100

    Imrpove classifiers, add dist list, bump version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='dacite',
-    version='0.0.1',
+    version='0.0.2',
     description='Simple creation of data classes from dictionaries.',
     author='Konrad Hałas',
     author_email='halas.konrad@gmail.com',
     url='https://github.com/konradhalas/dacite',
     license='MIT',
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='dataclasses validation',
     py_modules=['dacite'],
