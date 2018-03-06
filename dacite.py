d63a8ea4affe8f127ee3b5e8572185195cc69438
konradhalas <halas.konrad@gmail.com>
2018-03-06 12:11:53 +0100

    Add new features - rename, prefixes, cast, transform and optionals

--- dacite.py
+++ dacite.py
@@ -1,22 +1,76 @@
-from typing import Dict, Any, TypeVar, Type
+from typing import Dict, Any, TypeVar, Type, Union, Iterable, Callable, List
 
-from dataclasses import fields, MISSING, is_dataclass
+from dataclasses import fields, MISSING, is_dataclass, Field
 
 T = TypeVar('T')
 
 
-def make(data_class: Type[T], data: Dict[str, Any]) -> T:
+def make(data_class: Type[T],
+         data: Dict[str, Any],
+         rename: Dict[str, str] = None,
+         prefixes: Dict[str, str] = None,
+         cast: Iterable[str] = None,
+         transform: Dict[str, Callable[[Any], Any]] = None) -> T:
+    rename = rename or {}
+    prefixes = prefixes or {}
+    cast = cast or []
+    transform = transform or {}
     values = {}
     for field in fields(data_class):
         try:
-            value = data[field.name]
+            if field.name in prefixes:
+                value = _extract_nested_dict_for_prefix(prefixes[field.name], data)
+            else:
+                key_name = rename.get(field.name, field.name)
+                value = data[key_name]
+            if field.name in transform:
+                value = transform[field.name](value)
             if is_dataclass(field.type):
-                value = make(field.type, value)
+                value = make(
+                    data_class=field.type,
+                    data=value,
+                    rename=_extract_nested_dict(field, rename),
+                    prefixes=_extract_nested_dict(field, prefixes),
+                    cast=_extract_nested_list(field, cast),
+                    transform=_extract_nested_dict(field, transform),
+                )
+            elif field.name in cast:
+                value = field.type(value)
             elif not isinstance(value, field.type):
                 raise TypeError(f'wrong type for field {field.name} - should be {field.type.__name__} '
                                 f'instead of {type(value).__name__}')
             values[field.name] = value
         except KeyError:
-            if field.default == MISSING:
+            if _is_optional(field):
+                values[field.name] = None
+            elif field.default == MISSING:
                 raise ValueError(f'missing value for field {field.name}')
     return data_class(**values)
+
+
+def _extract_nested_dict(field: Field, params: Dict[str, Any]) -> Dict[str, Any]:
+    prefix = field.name + '.'
+    return _extract_nested_dict_for_prefix(prefix=prefix, data=params)
+
+
+def _extract_nested_dict_for_prefix(prefix: str, data: Dict[str, Any]) -> Dict[str, Any]:
+    result = {}
+    prefix_len = len(prefix)
+    for key, val in data.items():
+        if key.startswith(prefix):
+            result[key[prefix_len:]] = val
+    return result
+
+
+def _extract_nested_list(field: Field, params: List[str]) -> List[str]:
+    result = []
+    prefix = field.name + '.'
+    prefix_len = len(prefix)
+    for name in params:
+        if name.startswith(prefix):
+            result.append(name[prefix_len:])
+    return result
+
+
+def _is_optional(field: Field):
+    return type(field.type) == type(Union) and type(None) in field.type.__args__
--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pytest
 from dataclasses import dataclass
 
@@ -17,6 +19,17 @@ class B:
     a: A
 
 
+@dataclass
+class C:
+    b: B
+
+
+@dataclass
+class D:
+    s: Optional[str]
+    i: int
+
+
 def test_make_from_correct_data():
     result = make(A, {'s': 'test', 'i': 1, 'f': 1.0})
 
@@ -27,7 +40,6 @@ def test_make_from_correct_data():
 def test_make_with_default_value():
     result = make(A, {'s': 'test', 'i': 1})
 
-    assert isinstance(result, A)
     assert result == A(s='test', i=1, f=0.0)
 
 
@@ -37,13 +49,77 @@ def test_make_from_incorrect_data():
 
 
 def test_make_without_required_value():
-    with pytest.raises(ValueError):
+    with pytest.raises(ValueError, message='missing value for field i'):
         make(A, {'s': 'test'})
 
 
-def test_make_with_a_inner_data_class():
+def test_make_with_nested_data_class():
     result = make(B, {'s': 'test', 'a': {'s': 'test', 'i': 1}})
 
-    assert isinstance(result, B)
-    assert result.s == 'test'
-    assert result.a == A(s='test', i=1)
+    assert result == B(s='test', a=A(s='test', i=1))
+
+
+def test_make_with_additional_values():
+    result = make(A, {'s': 'test', 'i': 1, 'x': 'extra'})
+
+    assert result == A(s='test', i=1)
+
+
+def test_make_with_rename():
+    result = make(A, {'s': 'test', 'j': 1}, rename={'i': 'j'})
+
+    assert result == A(s='test', i=1)
+
+
+def test_make_with_nested_rename():
+    result = make(B, {'s': 'test', 'a': {'s': 'test', 'j': 1}}, rename={'a.i': 'j'})
+
+    assert result == B(s='test', a=A(s='test', i=1))
+
+
+def test_make_with_prefix():
+    result = make(B, {'s': 'test', 'a_s': 'test', 'a_i': 1}, prefixes={'a': 'a_'})
+
+    assert result == B(s='test', a=A(s='test', i=1))
+
+
+def test_make_with_nested_prefix():
+    result = make(C, {'b': {'s': 'test', 'a_s': 'test', 'a_i': 1}}, prefixes={'b.a': 'a_'})
+
+    assert result == C(b=B(s='test', a=A(s='test', i=1)))
+
+
+def test_make_with_prefix_and_rename():
+    result = make(B, {'s': 'test', 'a_s': 'test', 'a_j': 1}, prefixes={'a': 'a_'}, rename={'a.i': 'j'})
+
+    assert result == B(s='test', a=A(s='test', i=1))
+
+
+def test_make_with_optional_value():
+    result = make(D, {'i': 1})
+
+    assert result == D(s=None, i=1)
+
+
+def test_make_with_cast():
+    result = make(A, {'s': 'test', 'i': '1'}, cast=['i'])
+
+    assert result == A(s='test', i=1)
+
+
+def test_make_with_nested_cast():
+    result = make(B, {'s': 'test', 'a': {'s': 'test', 'i': '1'}}, cast=['a.i'])
+
+    assert result == B(s='test', a=A(s='test', i=1))
+
+
+def test_make_with_transform():
+    result = make(A, {'s': 'TEST', 'i': 1}, transform={'s': str.lower})
+
+    assert result == A(s='test', i=1)
+
+
+def test_make_with_nested_transform():
+    result = make(B, {'s': 'test', 'a': {'s': 'TEST', 'i': 1}}, transform={'a.s': str.lower})
+
+    assert result == B(s='test', a=A(s='test', i=1))
