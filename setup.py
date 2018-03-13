68dab83b62ecd746e5fbb6d40401d4807b9eb026
konradhalas <halas.konrad@gmail.com>
2018-03-13 23:49:49 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dacite',
-    version='0.0.5',
+    version='0.0.6',
     description='Simple creation of data classes from dictionaries.',
     author='Konrad Hałas',
     author_email='halas.konrad@gmail.com',
@@ -11,7 +11,7 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3.6',
     ],
-    keywords='dataclasses validation',
+    keywords='dataclasses',
     py_modules=['dacite'],
     install_requires=['dataclasses'],
 )
