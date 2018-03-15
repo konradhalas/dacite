840263b868f3bf7e805163fa8e6f58040e4bdf8b
konradhalas <halas.konrad@gmail.com>
2018-03-15 10:03:45 +0100

    Add new classifiers and bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except(IOError, ImportError):
 
 setup(
     name='dacite',
-    version='0.0.9',
+    version='0.0.10',
     description='Simple creation of data classes from dictionaries.',
     long_description=long_description,
     author='Konrad Hałas',
@@ -17,7 +17,13 @@ setup(
     url='https://github.com/konradhalas/dacite',
     license='MIT',
     classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     python_requires='>=3.6',
     keywords='dataclasses',
