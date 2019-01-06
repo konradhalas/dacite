9c3558a889e780e7faccf19b604a3aa8daf8a955
konradhalas <halas.konrad@gmail.com>
2019-01-06 15:16:08 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setup(
     name='dacite',
-    version='0.0.20',
+    version='0.0.21',
     description='Simple creation of data classes from dictionaries.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
