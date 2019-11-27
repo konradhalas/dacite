ed6d4df156d09b7d995dd295d5e3e9386de552da
konradhalas <halas.konrad@gmail.com>
2019-11-27 23:10:34 +0100

    Add Python 3.8 to Travis and classifiers

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=3.6",
