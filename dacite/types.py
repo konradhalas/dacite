d4f3d6077ab5ec0ee80049dabbe5e89782945090
konradhalas <halas.konrad@gmail.com>
2020-05-11 11:18:03 +0200

    Use consistent "type" type hints

--- dacite/types.py
+++ dacite/types.py
@@ -1,7 +1,7 @@
 from dataclasses import InitVar
 from typing import Type, Any, Optional, Union, Collection, TypeVar, Dict, Callable, Mapping, List
 
-NoneType: type = type(None)
+NoneType: Type = type(None)
 
 T = TypeVar("T", bound=Any)
 
