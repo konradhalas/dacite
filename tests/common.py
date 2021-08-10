793c1e0a76b1917530465c75aab332841f7e1443
Tobias Pfeiffer <tgp@preferred.jp>
2021-08-10 10:35:29 +0300

    do not treat generic numpy.ndarray as a generic collection allow `is_instance` checks for arbitrary generic types add test for `npt.NDArray` handling

--- tests/common.py
+++ tests/common.py
@@ -2,6 +2,7 @@ import sys
 
 import pytest
 
+ndarray_support = pytest.mark.skipif(sys.version_info < (3, 7), reason="requires Python 3.7")
 literal_support = init_var_type_support = pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python 3.8")
 type_hints_with_generic_collections_support = pytest.mark.skipif(
     sys.version_info < (3, 9), reason="requires Python 3.9"
