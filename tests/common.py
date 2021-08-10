0e378974cfe425d76cde4b285563825e64a47880
Tobias Pfeiffer <tgp@preferred.jp>
2021-08-10 17:03:34 +0900

    add test for npt.NDArray handling

--- tests/common.py
+++ tests/common.py
@@ -3,3 +3,4 @@ import sys
 import pytest
 
 literal_support = init_var_type_support = pytest.mark.skipif(sys.version_info < (3, 8), reason="requires Python 3.8")
+ndarray_support = pytest.mark.skipif(sys.version_info < (3, 7), reason="requires Python 3.7")
