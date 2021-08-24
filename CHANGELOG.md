a7834ba2421f6c52be8cd0f787660940deb9fc5b
konradhalas <halas.konrad@gmail.com>
2021-08-24 14:43:28 +0200

    Add explicit `__all__` configuration. Fixes #133

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Add explicit `__all__` configuration
+
 ### Fixed
 
 - Do not suppress `KeyError` in a type hook
--- dacite/__init__.py
+++ dacite/__init__.py
@@ -1,3 +1,25 @@
 from dacite.config import Config
 from dacite.core import from_dict
-from dacite.exceptions import *
+from dacite.exceptions import (
+    DaciteError,
+    DaciteFieldError,
+    WrongTypeError,
+    MissingValueError,
+    UnionMatchError,
+    StrictUnionMatchError,
+    ForwardReferenceError,
+    UnexpectedDataError,
+)
+
+__all__ = [
+    "Config",
+    "from_dict",
+    "DaciteError",
+    "DaciteFieldError",
+    "WrongTypeError",
+    "MissingValueError",
+    "UnionMatchError",
+    "StrictUnionMatchError",
+    "ForwardReferenceError",
+    "UnexpectedDataError",
+]
