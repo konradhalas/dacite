298b061539a5320ddef918ee70874354e46812fd
github-action-benchmark <github@users.noreply.github.com>
2022-12-22 16:05:27 +0000

    add Benchmark (pytest) benchmark result for a86ef2036b60e9a008714ac9d0919f18529203fc

---
+++ performance/3.10/data.js
@@ -0,0 +1,88 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1671725127950,
+  "repoUrl": "https://github.com/konradhalas/dacite",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "contact@mciszczon.pl",
+            "name": "Mateusz Ciszczoń",
+            "username": "mciszczon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a86ef2036b60e9a008714ac9d0919f18529203fc",
+          "message": "Add performance testing (#202)\n\n* Add pytest-benchmark to setup.py\r\n\r\n* Add pre-commit with black\r\n\r\n* Add pytest benchmark config to pyproject.toml\r\n\r\n* Update gitignore\r\n\r\n* Add performance tests\r\n\r\n* Add performance check to CI action\r\n\r\n* Rename publish to Publish\r\n\r\n* Remove performance Github Pages push\r\n\r\n* Set auto-push explicitly to false\r\n\r\n* Change gh-pages branch to master\r\n\r\n* Configure performance CI step further\r\n\r\n* Fix gh-pages for benchmark\r\n\r\n* Split gh-pages by Python version\r\n\r\n* Remove docs/benchmark dir\r\n\r\n* Add performance test for multiple items parsing\r\n\r\n* Update README.md with performance testing\r\n\r\n* Change GITHUB_TOKEN to BENCHMARK_TOKEN\r\n\r\n* Setup Github Actions not to publish performance on PR tests\r\n\r\n* Do not publish performance for PR runs, only compare performance to the master branch",
+          "timestamp": "2022-12-22T17:04:25+01:00",
+          "tree_id": "f5d80531a2bf409b0362d7802461fed1c2f9df81",
+          "url": "https://github.com/konradhalas/dacite/commit/a86ef2036b60e9a008714ac9d0919f18529203fc"
+        },
+        "date": 1671725126884,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/performance/test_performance.py::test_basic_scenario",
+            "value": 2788.0856778819325,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00010097620705123894",
+            "extra": "mean: 358.66903514948126 usec\nrounds: 1707"
+          },
+          {
+            "name": "tests/performance/test_performance.py::test_union_matching",
+            "value": 2092.73367715835,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00014058335506003528",
+            "extra": "mean: 477.84388950908703 usec\nrounds: 1792"
+          },
+          {
+            "name": "tests/performance/test_performance.py::test_strict_unions_match",
+            "value": 182.33652044345723,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0008861981070070033",
+            "extra": "mean: 5.484364830303434 msec\nrounds: 165"
+          },
+          {
+            "name": "tests/performance/test_performance.py::test_collection_of_union",
+            "value": 1452.4153658164475,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00048546415397584305",
+            "extra": "mean: 688.5082763069428 usec\nrounds: 1473"
+          },
+          {
+            "name": "tests/performance/test_performance.py::test_type_hooks",
+            "value": 2403.530541666502,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00008675206953266443",
+            "extra": "mean: 416.0546257534319 usec\nrounds: 1825"
+          },
+          {
+            "name": "tests/performance/test_performance.py::test_casting",
+            "value": 19227.870642596015,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00002627318389841662",
+            "extra": "mean: 52.00783896396064 usec\nrounds: 10153"
+          },
+          {
+            "name": "tests/performance/test_performance.py::test_forward_references",
+            "value": 1685.599319807805,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00024278830929571328",
+            "extra": "mean: 593.2607994372124 usec\nrounds: 1421"
+          },
+          {
+            "name": "tests/performance/test_performance.py::test_parsing_multiple_items",
+            "value": 84.52554078055277,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0013077314081634522",
+            "extra": "mean: 11.8307435925932 msec\nrounds: 81"
+          }
+        ]
+      }
+    ]
+  }
+}
