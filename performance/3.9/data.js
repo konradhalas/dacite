d1ba3eed9d02b6741faaca0928945390a7004579
github-action-benchmark <github@users.noreply.github.com>
2022-12-23 13:17:13 +0000

    add Benchmark (pytest) benchmark result for e9e99e831b7f71fcbae21b7dca9856684a7093f7

--- performance/3.9/data.js
+++ performance/3.9/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1671725122459,
+  "lastUpdate": 1671801433668,
   "repoUrl": "https://github.com/konradhalas/dacite",
   "entries": {
     "Benchmark": [
@@ -80,6 +80,86 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.0007150359785191927",
             "extra": "mean: 14.93577922222101 msec\nrounds: 63"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "contact@mciszczon.pl",
+            "name": "Mateusz Ciszczoń",
+            "username": "mciszczon"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e9e99e831b7f71fcbae21b7dca9856684a7093f7",
+          "message": "Fix tests (#203)\n\n* Fix performance tests to properly create dataclass instances\r\n\r\n* Add missing tests to have 100% coverage\r\n\r\n* Disable commenting on alerts",
+          "timestamp": "2022-12-23T14:16:17+01:00",
+          "tree_id": "8ca9f932da5b1e2e3634a8c70240a6b17f079c3b",
+          "url": "https://github.com/konradhalas/dacite/commit/e9e99e831b7f71fcbae21b7dca9856684a7093f7"
+        },
+        "date": 1671801429934,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "tests/performance/test_from_dict.py::test_basic_scenario",
+            "value": 2623.1421820818773,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00030969966589247644",
+            "extra": "mean: 381.22218720387554 usec\nrounds: 1688"
+          },
+          {
+            "name": "tests/performance/test_from_dict.py::test_union_matching",
+            "value": 61.5328925711401,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0005154380675382599",
+            "extra": "mean: 16.251470688524332 msec\nrounds: 61"
+          },
+          {
+            "name": "tests/performance/test_from_dict.py::test_strict_unions_match",
+            "value": 61.3211745375804,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0004342557622369309",
+            "extra": "mean: 16.307580661018726 msec\nrounds: 59"
+          },
+          {
+            "name": "tests/performance/test_from_dict.py::test_collection_of_union",
+            "value": 1457.534704988466,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000041186015656963954",
+            "extra": "mean: 686.0900097798449 usec\nrounds: 1227"
+          },
+          {
+            "name": "tests/performance/test_from_dict.py::test_type_hooks",
+            "value": 60.34281974065486,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00031145511782841795",
+            "extra": "mean: 16.571979968749595 msec\nrounds: 64"
+          },
+          {
+            "name": "tests/performance/test_from_dict.py::test_casting",
+            "value": 20152.546098190833,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00000475537379734313",
+            "extra": "mean: 49.62152152525152 usec\nrounds: 9756"
+          },
+          {
+            "name": "tests/performance/test_from_dict.py::test_forward_references",
+            "value": 60.81220661530606,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00047079489505880384",
+            "extra": "mean: 16.444067000001052 msec\nrounds: 59"
+          },
+          {
+            "name": "tests/performance/test_from_dict.py::test_parsing_multiple_items",
+            "value": 2.421872511904388,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0061868568270319595",
+            "extra": "mean: 412.90365000000406 msec\nrounds: 5"
           }
         ]
       }
