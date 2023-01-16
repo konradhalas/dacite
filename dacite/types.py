5d0efb5aaae4e8639a65c5eb2ead22c3c002ef84
Mateusz Ciszczoń <contact@mciszczon.pl>
2023-01-16 10:48:37 +0100

    Cache issubclass call

--- dacite/types.py
+++ dacite/types.py
@@ -230,7 +230,7 @@ def is_subclass(sub_type: Type, base_type: Type) -> bool:
     if is_generic_collection(sub_type):
         sub_type = extract_origin_collection(sub_type)
     try:
-        return issubclass(sub_type, base_type)
+        return cache(issubclass)(sub_type, base_type)
     except TypeError:
         return False
 
