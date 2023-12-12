df053ceb0927cf64904d3a4d5c4f4e7cfd25ed29
1stDimension <23144389+1stDimension@users.noreply.github.com>
2023-12-12 21:16:46 +0100

    Adding python 3.12 to setup and testing

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=3.6",
