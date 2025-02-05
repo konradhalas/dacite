daad7e4a15569321d3cf118f8083a71a65d7e044
Mateusz Ciszczoń <contact@mciszczon.pl>
2025-02-05 10:19:01 +0100

    Fix version in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="dacite",
-    version="1.9.1",
+    version="1.9.2",
     description="Simple creation of data classes from dictionaries.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
